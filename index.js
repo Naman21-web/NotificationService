d1331b62c1adfcde925f26e0086e1776baefcbae
Naman <jainn0984@gmail.com>
2026-03-13 22:42:24 +0530

    Added nodemailer config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const env = require("dotenv");
 const mongoose = require("mongoose");
+const {sendMail} = require("./services/email.service");
 
 env.config();
 
@@ -15,6 +16,7 @@ app.listen(process.env.PORT, async () => {
     try{
         await mongoose.connect(process.env.DB_URL);
         console.log("Successfully connected to mongo");
+        // sendMail("jainn0984@gmail.com","MBA Test Mail","This is test mail of our Movie Booking Application",false);
     }
     catch(error){
         console.log(error);
---
+++ services/email.service.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    // host: process.env.EMAIL_HOST,
+    // port: process.env.EMAIL_PORT,
+    // secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false, // 👈 required on your network
+    },
+});
+
+const sendMail = async (to, subject, content, isHtml = false) => {
+    const mailOptions = {
+        from: "Movie Booking App <" + process.env.EMAIL_USER + ">",
+        to,
+        subject,
+    };
+
+    if (isHtml) {
+        mailOptions.html = content;
+    } else {
+        mailOptions.text = content;
+    }
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully");
+    } catch (error) {
+        console.error("Error sending email:", error);
+    }
+};
+
+module.exports = {
+    sendMail
+}
